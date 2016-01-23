258afd2bb8e57a57b60bef47acfce74b860ee535
MengLingjie <meng.lj@gmail.com>
2016-01-23 21:38:42 +0800

    feat (download): 能够下载img了
    能够下载img内容了
    暂时使用pickle保存img信息，后续再增加数据库支持

--- downloadhtml.py
+++ downloadhtml.py
@@ -6,12 +6,15 @@ import re
 import gzip,StringIO
 import os
 import logging
+import pickle,hashlib
 
 logging.basicConfig(filename="log.txt",
                     format='%(asctime)s %(message)s',
                     datefmt='%Y-%m-%d %I:%M:%S %p',
                     level=logging.INFO)
 
+IMG_DB_FILE_NAME='imgs.db'
+IMG_DIR_NAME='imgs/'
 
 def getHtmlString(response):
     html_str = response.read()
@@ -41,8 +44,41 @@ def getlinks(host, url, html_str, pattern):
                 links.append(linkstring['href'][len('http://'+host):])
 
     logging.debug("Get %s links : %s", url, links)
+    return links
+
+# 取得网页中的img超链接（绝对路径）
+def getimglinks(host, url, html_str):
+    links=[]
+    soup = BeautifulSoup.BeautifulSoup(html_str,"html.parser")
+    imglinks = soup.find_all(name='img')
+    for linkstring in imglinks:
+        links.append(linkstring['src'])
 
     return links
+
+def imgexist(url):
+    #查找数据库，看url是否存在在数据库中
+    return pickle.load(open(IMG_DB_FILE_NAME,'r')).has_key(url)
+
+def getimgfile(url):
+    #下载url图片，计算hash值，保存到缓存目录中。将url和hash值信息记录到数据库中。
+    response = urllib2.urlopen(url)
+    s1 = hashlib.sha1()
+    buf = response.read()
+    s1.update(buf)
+    imgname = s1.hexdigest()
+    logging.debug("Img hash : %s",imgname)
+
+    imgpath = IMG_DIR_NAME+imgname[0:2]
+    if not os.path.exists(imgpath):
+        os.makedirs(imgpath)
+
+    with open(imgpath+'/'+imgname[2:], 'wb') as f:
+        f.write(buf)
+        imginfodict = pickle.load(open(IMG_DB_FILE_NAME,'r'))
+        imginfodict[url] = imgname
+        pickle.dump(imginfodict,open(IMG_DB_FILE_NAME,'w'))
+    pass
 
 def writehtmlfile(host, url, str):
 
@@ -76,6 +112,11 @@ def getHtmlFile(prot, host, url, pattern):
             for link in links :
                 if (not fileDownloaded(host, link)) :
                     getHtmlFile(prot, host, link, pattern)
+
+            imglinks = getimglinks(host, url, html_str)
+            for link in imglinks:
+                if (not imgexist(link)):
+                    getimgfile(link)
         except BaseException as e:
             logging.error("Error : %s", e)
             print("Error : %s"%e)
@@ -84,4 +125,5 @@ def getHtmlFile(prot, host, url, pattern):
 def getWebSite(args):
     getHtmlFile(args[0],args[1],args[2],args[3])
 
-#getHtmlFile("http://", "www.runoob.com","/python/python-tutorial.html","^/python")
+#getHtmlFile("http://", "www.runoob.com","/python/python-tutorial.html","^/python/python-tutorial.html")
+getHtmlFile("http://", "www.huihui.cn","/all?page=1","^/all\?page=1")
